1fbd93236820f36380e3470296b106b342e0739d
Andrew Barrett <brazenbarrett@gmail.com>
2017-10-05 14:22:52 -0500

    Final touches, all functionailty implemented

--- main.py
+++ main.py
@@ -1,38 +1,54 @@
+# Questions for class: How do I return seperate errors when rendering my template?
+# How do I make user input persist through attempted form submission? The example does not do this.
+
 from flask import Flask, request, redirect, render_template
 
 app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
-@app.route('/welcome', methods=['GET', 'POST'])
-def welcome():
+@app.route('/signup', methods=['GET','POST'])
+def validate_userdata():
     username = request.form['username']
     password = request.form['password']
-    vp = request.form['verify-password']
+    verify = request.form['verify']
     email = request.form['email']
-    food = request.form['fav-food']
-    usernamerror = ''
+
+    usernameerror = ''
     passworderror = ''
+    verifyerror = ''
     emailerror = ''
 
-    if username == ('') or (not username) or (username.strip() == "") :
+    if username == ('') or (not username) or (username.strip() == ''):
         usernameerror = "Please enter a username"
-        return redirect("/?error=" + usernameerror)
-    if ' ' in username:
+    elif ' ' in username:
         usernameerror = "Usernames cannot contain spaces"
-        return redirect("/?error=" + usernameerror)
-    if len(username) < 3 or len(username) > 19:
+    elif len(username) < 3 or len(username) > 19:
         usernameerror = "Usernames must be at least 3 characters and fewer than 20"
-        return redirect("/?error=" + usernameerror)
-    # if password == ('') or (not password) or (username.strip() == "") :
-    #     passworderror = "Please enter a password"
-    #     return redirect("/?error=" + passworderror)
 
-    return render_template('welcome.html', username=username)
+    if password == ('') or (not password) or (password.strip() == ''):
+        passworderror = "Please enter a password"
+    elif ' ' in password:
+        passworderror = "Passwords cannot contain spaces"
+    elif len(password) < 3 or len(password) > 19:
+        passworderror = "Passwords must be at least 3 characters and fewer than 20"
+    
+    if verify != password or verify == '' or " " in verify:
+        verifyerror = "Passwords do not match"
 
-@app.route('/')
-def validate_userdata():
-    encoded_error = request.args.get("error")
-    return render_template('user-login.html', error=encoded_error)
+    if email.count("@") != 1 and email.count(".") != 1:
+        emailerror = "Invalid email"
+        
+    
+    if (usernameerror != '') or (passworderror != '') or (verifyerror != '') or (emailerror != ''):
+        return render_template("signup.html", usernameerror=usernameerror, passworderror=passworderror,
+        verifyerror=verifyerror, emailerror=emailerror, username=username, email=email)
+
+    else:
+        return render_template("welcome.html", username=username)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template("signup.html")
 
 app.run()
